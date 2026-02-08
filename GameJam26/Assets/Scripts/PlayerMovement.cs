8b26d65cd73e88a7a14f69b872da96c7ef75bb6d
Tuymuo <144937157+Tuymuo@users.noreply.github.com>
2026-02-08 16:12:17 +0100

    Movimiento

--- GameJam26/Assets/Scripts/PlayerMovement.cs
+++ GameJam26/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,12 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,10 +22,14 @@ public class PlayerMovement : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal"); // A/D or left/right arrow
         float z = Input.GetAxisRaw("Vertical");   // W/S or up/down arrow
-
+        
         Vector3 input = new Vector3(x, 0f, z).normalized;
         Vector3 velocity = input * moveSpeed;
 
         rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+
+        // Conectar con el Animator
+        bool isMoving = input.magnitude > 0;
+        animator.SetBool("isMoving", isMoving);   
     }
 }
