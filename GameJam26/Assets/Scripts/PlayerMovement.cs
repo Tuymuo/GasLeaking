fb69ed57f058c3984961c949e7ac5af12e341ab4
FranciscoCampraBautista <francisco.campra@firescalestudios.com>
2026-02-08 16:25:34 +0100

    Flip
    flip added

--- GameJam26/Assets/Scripts/PlayerMovement.cs
+++ GameJam26/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private bool facingRight = true;
 
     private Rigidbody rb;
 
@@ -23,7 +24,22 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 input = new Vector3(x, 0f, z).normalized;
         Vector3 velocity = input * moveSpeed;
-
+        
         rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+
+        if (x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
